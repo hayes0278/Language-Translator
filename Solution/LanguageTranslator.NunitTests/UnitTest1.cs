3d075237ae5687e0215d4c2b98d23e81845e816f
Marshall Hayes <hayes0278@users.noreply.github.com>
2025-10-06 17:03:28 -0500

    Added initial scaffolding.

---
+++ Solution/LanguageTranslator.NunitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace LanguageTranslator.NunitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Solution/LanguageTranslator.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LanguageTranslator.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
